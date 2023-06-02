6fa4f76fe280cdc959a2286ca13007429efb4943
Therich99 <richard.bejarano@unillanos.edu.co>
2023-06-02 18:32:29 -0500

    pruebas de tipo de documento con persona

--- api/filters.py
+++ api/filters.py
@@ -2,7 +2,17 @@ from django_filters import rest_framework as filters
 import django_filters
 from .models import Tarea,Persona
 
+from django.db.models import CharField
+from django.db.models.functions import Cast
+from django.db.models import Value
+
+
 class TareaFilter(filters.FilterSet):
+    tipo_documento = filters.CharFilter(method='filter_tipo_documento')
+
+    def filter_tipo_documento(self, queryset, name, value):
+        return queryset.annotate(persona_documento=Cast('persona__documento', CharField())).filter(persona_documento__icontains=value)
+
 
     class Meta:
         model = Tarea
@@ -14,6 +24,17 @@ class TareaFilter(filters.FilterSet):
         }
 
 
+""" class TareaFilter(filters.FilterSet):
+    tipo_documento = filters.CharFilter(method='filter_tipo_documento')
+
+    def filter_tipo_documento(self, queryset, name, value):
+        return queryset.annotate(persona_documento=Cast('persona__documento', CharField())).filter(persona_documento__icontains=value)
+
+    class Meta:
+        model = Tarea
+        fields = ['tipo_documento'] """
+
+        
 
 #?fecha_limite__gte=##-##-##&fecha_limite__lte=###-##-##
 class PersonaFilter(filters.FilterSet):
